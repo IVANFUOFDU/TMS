3681c0c6e8466a0d78b5b1b5967804383865962a
Ivan <ivanfuofdu@gmail.com>
2025-04-05 01:03:01 +0300

    [add] Added some logic of CLI.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -12,10 +14,10 @@ public class Main {
             int choice = getIntInput("Choose: ");
 
             switch (choice) {
-                case 1 -> System.out.println("Dummy operation");
-                case 2 -> System.out.println("Dummy operation");
-                case 3 -> System.out.println("Dummy operation");
-                case 4 -> System.out.println("Dummy operation");
+                case 1 -> addTask();
+                case 2 -> listTask();
+                case 3 -> updateTask();
+                case 4 -> deleteTask();
                 case 5 -> running = false;
                 default -> System.out.println("Invalid choice. Retry.");
             }
@@ -41,5 +43,50 @@ public class Main {
                 System.out.println("Invalid input. Retry.");
             }
         }
+    }
+
+    public static void addTask() {
+        System.out.println("\n---> Add Task <---");
+    }
+
+    public static void listTask() {
+        List<Task> tasks = manager.getAll();
+        System.out.println("\n---> List Tasks <---");
+        if (tasks.isEmpty()) {
+            System.out.println("There are not tasks.");
+            return;
+        }
+
+        for (Task task : tasks) {
+            System.out.printf(
+                    "[ID: %d] %s - %s | Due: %s | Category: %s | Priority: %s | Status: %s%n",
+                    task.getId(),
+                    task.getTitle(),
+                    task.getDescription(),
+                    task.getDate(),
+                    task.getCategory(),
+                    task.getPriority(),
+                    task.isComplete() ? "Complete" : "Incomplete"
+            );
+        }
+    }
+
+    public static void updateTask() {
+        int id = getIntInput("Enter id to update: ");
+        Optional<Task> taskOptional = manager.getById(id);
+
+        if (taskOptional.isEmpty()) {
+            System.out.println("Task not found.");
+            return;
+        }
+
+        System.out.println("\n---> Update Task <---");
+    }
+
+    public static void deleteTask() {
+        System.out.println("\n---> Delete Task <---");
+        int id = getIntInput("Enter ID to delete: ");
+        boolean deleted = manager.delete(id);
+        System.out.println(deleted ? "Deleted successfully" : "Task not found");
     }
 }
